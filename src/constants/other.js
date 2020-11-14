e163d3eca5c75f75030e697363a031b49f7a2104
Max Bezvugliak <maximbezvugliak@gmail.com>
2020-11-14 12:26:17 +0200

    add api by elastic ip

--- src/constants/other.js
+++ src/constants/other.js
@@ -1,2 +1,2 @@
-export const SERVER_ENDPOINT = 'http://localhost:5000/';
+export const SERVER_ENDPOINT = 'http://18.157.193.128:5000/';
 export const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@$%^*()_+";
