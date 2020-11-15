a7f41e4386abb7f97fd8dbad38549b8d59f07343
Max Bezvugliak <maximbezvugliak@gmail.com>
2020-11-15 22:30:28 +0200

    hh

--- src/constants/other.js
+++ src/constants/other.js
@@ -1,2 +1,2 @@
-export const SERVER_ENDPOINT = 'http://18.157.193.128:5000/api/v1/';
+export const SERVER_ENDPOINT = 'http://18.157.193.128/api/v1/';
 export const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@$%^*()_+";
